6000d6f0852364e1e903e52a8979330f2161b5d9
Heiko Plümer <hpr@immocloud.de>
2024-09-22 17:44:06 +0200

    Replace old project with new one

--- components/PricingCard.tsx
+++ components/PricingCard.tsx
@@ -70,7 +70,7 @@ export default function PricingCard({ title, price, description, features, testT
             )}
             { !highlighted && (
                 <Button
-                    variant="primary"
+                    variant="default"
                 >
                     {testText}
                 </Button>
