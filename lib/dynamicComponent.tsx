5de4adb8284f764dbfc494f06717b1326c65e69d
Heiko Plümer <hpr@immocloud.de>
2024-09-22 18:09:30 +0200

    Replace old project with new one

--- lib/dynamicComponent.tsx
+++ lib/dynamicComponent.tsx
@@ -1,18 +1,35 @@
 import PricingCard from "@/components/PricingCard";
 import PricetableContainer from "@/components/PricetableContainer";
 
-interface BlockComponentProps {
+interface PricingCardBlock {
     _uid: string;
-    component: 'PricingCard' | 'PricetableContainer';
-    [key: string]: unknown;
+    component: 'PricingCard';
+    title: string;
+    price: string;
+    description: string;
+    features: string[];
+    testText: string;
+    highlighted: boolean;
 }
+
+interface PricetableContainerBlock {
+    _uid: string;
+    component: 'PricetableContainer';
+    title: string;
+    subtitle: string;
+    description: string;
+    ctaText: string;
+    pricingCards: PricingCardBlock[];
+}
+
+type Block = PricingCardBlock | PricetableContainerBlock;
 
 const Components = {
     PricingCard,
     PricetableContainer,
 };
 
-const DynamicComponent = ({ blok }: { blok: BlockComponentProps }) => {
+const DynamicComponent = ({ blok }: { blok: Block }) => {
     const Component = Components[blok.component];
     if (Component) {
         return <Component {...blok} />;
